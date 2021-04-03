a764bc22b41e27003080e2a8b4f22f03e56c0e95
Daniel Boyle <boylede+github@gmail.com>
2021-04-02 23:28:25 -0700

    add first stub gui

--- vhr_gui/src/main.rs
+++ vhr_gui/src/main.rs
@@ -1,4 +1,9 @@
+use druid::{AppLauncher, Widget, WindowDesc, widget::{
+    Align, Flex, Label,
+}};
+
 use std::io::Write;
+
 use vhr_chardata::LoadedCharacter;
 use vhr_serde::ser::to_bytes;
 
@@ -18,4 +23,21 @@ fn main() {
         let e = serialized.unwrap_err();
         println!("error while serializing: {:?}", e);
     }
+
+    let main_window = WindowDesc::new(build_root_widget)
+        .title("vhc_reader")
+        .window_size((500.0, 800.0));
+
+    AppLauncher::with_window(main_window)
+        .use_simple_logger()
+        .launch(my_char)
+        .expect("Failed to launch application.");
+
+}
+
+fn build_root_widget() -> impl Widget<LoadedCharacter> {
+    Align::centered(
+        Flex::column()
+            .with_child(Label::new("Root")
+    ))
 }
