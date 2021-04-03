138835adb2f690b15032e4a1bcea67ee646ff9ee
Daniel Boyle <boylede+github@gmail.com>
2021-04-02 23:42:12 -0700

    slightly different visdata serialization implementation

--- vhr_chardata/src/vis_data.rs
+++ vhr_chardata/src/vis_data.rs
@@ -1,5 +1,5 @@
 use druid::{Data, Lens};
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize, Serializer, Deserializer};
 use serde_repr::{Deserialize_repr, Serialize_repr};
 use std::rc::Rc;
 
@@ -9,16 +9,48 @@ const FOG_LENGTH: usize = MAP_WIDTH * MAP_WIDTH;
 #[derive(PartialEq, Eq, Data, Clone, Lens, Debug, Deserialize, Serialize)]
 pub struct VisibilityData {
     four: u32,
-    twenty_fourty_eight: u32,
-    fog: Rc<Vec<u8>>,
+    // twenty_fourty_eight: u32,
+    fog: Rc<Vec<VisibilityRow>>,
+}
+
+impl VisibilityData {
+    pub fn pre_serialize(&mut self) -> usize {
+        // (*self.fog).len() * MAP_WIDTH + 4
+        0x400008
+    }
+}
+
+#[derive(PartialEq, Eq, Clone, Lens, Debug, Deserialize)]
+pub struct VisibilityRow {
+    inner: Vec<u8>,
+}
+
+impl Default for VisibilityRow {
+    fn default() -> Self {
+        VisibilityRow {
+            inner: vec![0;MAP_WIDTH],
+        }
+    }
 }
 
 impl Default for VisibilityData {
     fn default() -> Self {
         VisibilityData {
             four: 4,
-            twenty_fourty_eight: 2048,
-            fog: Rc::new(vec![0u8;FOG_LENGTH]),
+            fog: Rc::new(vec![VisibilityRow::default();MAP_WIDTH]),
         }
     }
+}
+
+impl Serialize for VisibilityRow {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer {
+            use serde::ser::SerializeTuple;
+            let mut tup = serializer.serialize_tuple(self.inner.len())?;
+            for byte in self.inner.iter() {
+                tup.serialize_element(byte);
+            }
+            tup.end()
+        }
 }
