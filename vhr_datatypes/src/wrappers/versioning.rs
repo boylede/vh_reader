4c387766c2ce34feccd3c4192d00e60a7c302d84
Daniel Boyle <boylede+github@gmail.com>
2021-11-01 23:13:03 -0400

    disallow deserializing invalid versions, but dont crash

--- vhr_datatypes/src/wrappers/versioning.rs
+++ vhr_datatypes/src/wrappers/versioning.rs
@@ -9,13 +9,31 @@ pub mod skills;
 
 /// an unknown data version
 /// provides a stub so deserialization can fail
-/// todo: write (de)serializer to always fail.
-#[derive(PartialEq, Debug, Serialize, Deserialize, Clone, Copy)]
+/// todo: write serializer to always fail.
+#[derive(PartialEq, Debug, Serialize, Clone, Copy)]
 pub struct UnknownVersion;
+
+impl<'de> Deserialize<'de> for UnknownVersion {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        Err(D::Error::custom("This version is not known"))
+    }
+}
 
 /// a version of the data structure known to not exist
 /// e.g. 0 isnt used as a version as far as i can tell
 /// todo: remove this boilerplate with changes to serialization so
 /// these impossible versions don't need to be represented here
-#[derive(PartialEq, Debug, Serialize, Deserialize, Clone, Copy)]
+#[derive(PartialEq, Debug, Serialize, Clone, Copy)]
 pub struct NoSuchVersion;
+
+impl<'de> Deserialize<'de> for NoSuchVersion {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        Err(D::Error::custom("This type is not valid"))
+    }
+}
