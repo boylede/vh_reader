5bfe7fae5de484dc514b8e81bc8af5dad682025f
Daniel Boyle <boylede+github@gmail.com>
2021-11-17 21:58:44 -0500

    rename egui to gui

--- vhr_gui/src/main.rs
+++ vhr_gui/src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     println!("reading files from {:?}", default_path);
 
     // let app = vhr_egui::FolderViewer::with_data_path(default_path);
-    let app = vhr_egui::CharacterEditor::with_data_path(default_path);
+    let app = vhr_gui::CharacterEditor::with_data_path(default_path);
     // let app = vhr_egui::MyApp::default();
     let options = eframe::NativeOptions {
         drag_and_drop_support: true,
